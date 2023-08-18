748970c16b8961576ed02a0a5f7c609018e9b3c6
jonny201251 <jonny201251@163.com>
2023-08-18 14:11:07 +0800

    aa

--- src/pages/ProviderScore1/CheckForm.jsx
+++ src/pages/ProviderScore1/CheckForm.jsx
@@ -149,12 +149,12 @@ export default (props) => {
                 maximum: 10,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 9,
-                maximum: 10,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 9,
+              maximum: 10,
+              required: true,
+            })*/
           } else if (itemValue === '有,但风险较小在可控范围内') {
             field.value = '6-8分';
             startScoreField &&
@@ -163,12 +163,12 @@ export default (props) => {
                 maximum: 8,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 6,
-                maximum: 8,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 6,
+              maximum: 8,
+              required: true,
+            })*/
           } else if (itemValue === '有,且风险较大') {
             field.value = '<6分';
             startScoreField &&
@@ -177,12 +177,12 @@ export default (props) => {
                 maximum: 6,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 0,
-                maximum: 6,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 0,
+              maximum: 6,
+              required: true,
+            })*/
           }
           if (session.getItem('user').loginName === '祁瑛') {
             endScoreField?.setPattern('editable');
@@ -199,12 +199,12 @@ export default (props) => {
                 maximum: 10,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 9,
-                maximum: 10,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 9,
+              maximum: 10,
+              required: true,
+            })*/
           } else if (itemValue === '有,影响较小') {
             field.value = '6-8分';
             startScoreField &&
@@ -213,12 +213,12 @@ export default (props) => {
                 maximum: 8,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 6,
-                maximum: 8,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 6,
+              maximum: 8,
+              required: true,
+            })*/
           } else if (itemValue === '有,影响较大') {
             field.value = '<6分';
             startScoreField &&
@@ -227,12 +227,12 @@ export default (props) => {
                 maximum: 6,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 0,
-                maximum: 6,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 0,
+              maximum: 6,
+              required: true,
+            })*/
           }
           if (
             session.getItem('user').loginName === '孙欢' ||
@@ -252,12 +252,12 @@ export default (props) => {
                 maximum: 10,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 9,
-                maximum: 10,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 9,
+              maximum: 10,
+              required: true,
+            })*/
           } else if (itemValue === '500万>注册资本≥100万') {
             field.value = '6-8分';
             startScoreField &&
@@ -266,12 +266,12 @@ export default (props) => {
                 maximum: 8,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 6,
-                maximum: 8,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 6,
+              maximum: 8,
+              required: true,
+            })*/
           } else if (itemValue === '注册资本<100万') {
             field.value = '<6分';
             startScoreField &&
@@ -280,12 +280,12 @@ export default (props) => {
                 maximum: 6,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 0,
-                maximum: 6,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 0,
+              maximum: 6,
+              required: true,
+            })*/
           }
           if (
             session.getItem('user').loginName === '孙欢' ||
@@ -305,12 +305,12 @@ export default (props) => {
                 maximum: 10,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 9,
-                maximum: 10,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 9,
+              maximum: 10,
+              required: true,
+            })*/
           } else if (itemValue === '与公司首次合作，配合度存在提升空间') {
             field.value = '6-8分';
             startScoreField &&
@@ -319,12 +319,12 @@ export default (props) => {
                 maximum: 8,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 6,
-                maximum: 8,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 6,
+              maximum: 8,
+              required: true,
+            })*/
           } else if (itemValue === '与公司合作深度较低配合度较差') {
             field.value = '<6分';
             startScoreField &&
@@ -333,12 +333,12 @@ export default (props) => {
                 maximum: 6,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 0,
-                maximum: 6,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 0,
+              maximum: 6,
+              required: true,
+            })*/
           }
           if (
             session.getItem('user').loginName === '孙欢' ||
@@ -358,12 +358,12 @@ export default (props) => {
                 maximum: 10,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 8,
-                maximum: 10,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 8,
+              maximum: 10,
+              required: true,
+            })*/
           } else if (itemValue === '按约定供货及时,采购主体使用情况一般') {
             field.value = '6-7分';
             startScoreField &&
@@ -372,12 +372,12 @@ export default (props) => {
                 maximum: 7,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 6,
-                maximum: 7,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 6,
+              maximum: 7,
+              required: true,
+            })*/
           } else if (itemValue === '未按约定及时供货,采购主体使用情况较差') {
             field.value = '<6分';
             startScoreField &&
@@ -386,12 +386,12 @@ export default (props) => {
                 maximum: 6,
                 required: true,
               });
-            endScoreField &&
-              endScoreField.setValidator({
-                minimum: 0,
-                maximum: 6,
-                required: true,
-              });
+            /*            endScoreField &&
+            endScoreField.setValidator({
+              minimum: 0,
+              maximum: 6,
+              required: true,
+            })*/
           }
           if (
             session.getItem('user').loginName === '孙欢' ||
