2fa4ced10a991948bdf746c9204dbc4ae160b24e
jonny201251 <jonny201251@163.com>
2023-09-22 17:49:43 +0800

    aa

--- src/pages/ProjectEnd/List.jsx
+++ src/pages/ProjectEnd/List.jsx
@@ -1,11 +1,10 @@
-import { projectEndPath } from '../../utils'
-import { BaseProTable2 } from '../../components'
+import { projectEndPath } from '../../utils';
+import { BaseProTable2 } from '../../components';
 
 export default () => {
   const columns = [
     { title: '项目类别', dataIndex: 'type', valueType: 'text' },
     { title: '项目名称', dataIndex: 'name', valueType: 'text' },
-    { title: '项目性质', dataIndex: 'name', valueType: 'text' },
     { title: '日期', dataIndex: 'date', valueType: 'text' },
     { title: '合同编号', dataIndex: 'a1', valueType: 'text' },
     { title: '合同金额', dataIndex: 'a1', valueType: 'text' },
@@ -14,7 +13,7 @@ export default () => {
     { title: '公司利润', dataIndex: 'a4', valueType: 'text' },
     { title: '事业部利润', dataIndex: 'a5', valueType: 'text' },
     { title: '备注', dataIndex: 'remark', valueType: 'text' },
-  ]
+  ];
 
-  return <BaseProTable2 path={projectEndPath} columns={columns}/>
-}
+  return <BaseProTable2 path={projectEndPath} columns={columns} />;
+};
--- src/pages/ProjectInOut/List.jsx
+++ src/pages/ProjectInOut/List.jsx
@@ -1,76 +1,113 @@
-import { contextPath, projectInOutPath, proTableRequest } from '../../utils'
-import ProTable from '@ant-design/pro-table'
-import { useRef } from 'react'
-import { FormDialog } from '@formily/antd'
+import { contextPath, projectInOutPath, proTableRequest } from '../../utils';
+import ProTable from '@ant-design/pro-table';
+import { useRef } from 'react';
+import { FormDialog } from '@formily/antd';
 
 export default () => {
-  const actionRef = useRef()
+  const actionRef = useRef();
 
   const render = (text, record) => {
     return [
-      <a onClick={async () => {
-        let dialog = FormDialog({
-            style: { top: 20 },
-            title: '项目收支表',
-            footer: null,
-            keyboard: false,
-            maskClosable: false,
-            width: '98%',
-          },
-          (form) => {
-            return <iframe
-              src={contextPath + '/jmreport/view/679927289940082688?projectId=' + record.projectId}
-              style={{ border: 0, width: '100%', height: document.body.clientHeight - 100 }}
-              frameBorder="0"/>
-          },
-        )
-        dialog.open({})
-      }}>{'项目收支表'}</a>,
-      <a onClick={async () => {
-        let dialog = FormDialog({
-            style: { top: 20 },
-            title: '收支明细表',
-            footer: null,
-            keyboard: false,
-            maskClosable: false,
-            width: '98%',
-          },
-          (form) => {
-            return <iframe
-              src={contextPath + '/jmreport/view/679996015666331648?projectId=' + record.projectId}
-              style={{ border: 0, width: '100%', height: document.body.clientHeight - 100 }}
-              frameBorder="0"/>
-          },
-        )
-        dialog.open({})
-      }}>{'收支明细表'}</a>,
-    ]
-  }
+      <a
+        onClick={async () => {
+          let dialog = FormDialog(
+            {
+              style: { top: 20 },
+              title: '项目收支表',
+              footer: null,
+              keyboard: false,
+              maskClosable: false,
+              width: '98%',
+            },
+            (form) => {
+              return (
+                <iframe
+                  src={
+                    contextPath +
+                    '/jmreport/view/679927289940082688?projectId=' +
+                    record.projectId
+                  }
+                  style={{
+                    border: 0,
+                    width: '100%',
+                    height: document.body.clientHeight - 100,
+                  }}
+                  frameBorder="0"
+                />
+              );
+            },
+          );
+          dialog.open({});
+        }}
+      >
+        {'项目收支表'}
+      </a>,
+      <a
+        onClick={async () => {
+          let dialog = FormDialog(
+            {
+              style: { top: 20 },
+              title: '收支明细表',
+              footer: null,
+              keyboard: false,
+              maskClosable: false,
+              width: '98%',
+            },
+            (form) => {
+              return (
+                <iframe
+                  src={
+                    contextPath +
+                    '/jmreport/view/679996015666331648?projectId=' +
+                    record.projectId
+                  }
+                  style={{
+                    border: 0,
+                    width: '100%',
+                    height: document.body.clientHeight - 100,
+                  }}
+                  frameBorder="0"
+                />
+              );
+            },
+          );
+          dialog.open({});
+        }}
+      >
+        {'收支明细表'}
+      </a>,
+    ];
+  };
 
   const columns = [
     { title: '项目名称', dataIndex: 'name', valueType: 'text' },
     { title: '任务号', dataIndex: 'taskCode', valueType: 'text' },
     { title: 'WBS编号', dataIndex: 'wbs', valueType: 'text' },
-    { title: '项目性质', dataIndex: 'property', valueType: 'text' },
     { title: '客户名称', dataIndex: 'customerName', valueType: 'text' },
     { title: '收款合同编号', dataIndex: 'contractCode', valueType: 'text' },
     { title: '合同名称', dataIndex: 'contractName', valueType: 'text' },
     { title: '合同额', dataIndex: 'contractMoney', valueType: 'text' },
-    { title: '操作', valueType: 'option', render: (text, record) => render(text, record) },
-  ]
+    {
+      title: '操作',
+      valueType: 'option',
+      render: (text, record) => render(text, record),
+    },
+  ];
 
-  return <ProTable
-    bordered
-    rowKey="id"
-    actionRef={actionRef}
-    columns={columns}
-    columnEmptyText={true}
-    // pagination={{ pageSize: 1}}
-    //列表数据
-    params={{ list: projectInOutPath.list }}
-    request={proTableRequest}
-    //
-    options={{ density: false }}
-    search={{ span: 6 }}
-  />
-}
+  return (
+    <ProTable
+      bordered
+      rowKey="id"
+      actionRef={actionRef}
+      columns={columns}
+      columnEmptyText={true}
+      // pagination={{ pageSize: 1}}
+      //列表数据
+      params={{ list: projectInOutPath.list }}
+      request={proTableRequest}
+      //
+      options={{ density: false }}
+      search={{ span: 6 }}
+    />
+  );
+};
